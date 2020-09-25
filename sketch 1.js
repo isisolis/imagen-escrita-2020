bcafbadb24910cac1b437297b5b2ff0ede94d26e
isisolis <69992018+isisolis@users.noreply.github.com>
2020-09-24 22:52:39 -0300

    retrato 1
    1

---
+++ sketch 1.js
@@ -0,0 +1,44 @@
+
+function setup() {
+  createCanvas(550, 650);
+}
+
+function draw() {
+  background("pink");
+  fill("black");
+  ellipse(230,150,170,200);
+  rect(145, 140, 169, 200);
+  fill(235,152,78);
+  rect(160,100,140,150,300);
+  fill("white");
+  ellipse(200,155,50,40);
+  ellipse(250,155,50,40);
+  fill(450,119,14);
+  ellipse(200,155,20,20);
+  ellipse(250,155,20,20);
+  fill(235,152,78);
+  ellipse(230,190,40,15);
+  curve(200,1,200,200,260,210,20,200);
+  
+
+  ellipse(230,330,400,35)
+  rect(142, 300, 175, 00);
+  ellipse(230,330,40,165)
+  
+  ellipse(172,600,50,300)
+  ellipse(282,600,50,300)
+ 
+  ellipse(440,330,39,30)
+  ellipse(20,330,39,30)
+  fill("blue");
+  arc(230, 520, 230, 470,PI, TWO_PI);
+
+  
+  
+  
+  
+    
+    
+  
+ 
+  }
